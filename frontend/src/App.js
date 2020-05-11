49e2e2a9082551a577fb1fa134956372dba11835
CaNopyEU <outcast.slovakia@gmail.com>
2020-05-11 17:22:00 +0200

    lection no.12: adding basic css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,22 @@ import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/Events";
 import BookingPage from "./pages/Booking";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 import './App.css';
 
 function App() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Redirect from="/" to="/auth" exact/>
-                <Route path="/auth" component={AuthPage}/>
-                <Route path="/events" component={EventsPage}/>
-                <Route path="/bookings" component={BookingPage}/>
-            </Switch>
+            <MainNavigation/>
+            <main className="main-content">
+                <Switch>
+                    <Redirect from="/" to="/auth" exact/>
+                    <Route path="/auth" component={AuthPage}/>
+                    <Route path="/events" component={EventsPage}/>
+                    <Route path="/bookings" component={BookingPage}/>
+                </Switch>
+            </main>
         </BrowserRouter>
     );
 }
