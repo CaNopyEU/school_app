cb3bdc57dc83a94adef3cbdebd4bcf1b7e944550
CaNopyEU <outcast.slovakia@gmail.com>
2020-05-11 16:50:00 +0200

    lection no.11: adding front-end structure

---
+++ frontend/src/pages/Booking.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class BookingPage extends Component {
+    render() {
+        return (
+            <h1>The Bookings Page</h1>
+        );
+    }
+}
+
+export default BookingPage;
